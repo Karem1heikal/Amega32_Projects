dd21520b26d241c776bcf495b02d7fa4b32957fd
Karem1heikal <147204895+Karem1heikal@users.noreply.github.com>
2025-01-24 03:31:56 +0200

    Add files via upload

---
+++ smart-home-system/smart-home-system-main/SYS_smart1/smartAPP.h
@@ -0,0 +1,24 @@
+/*
+ * smartAPP.h
+ *
+ *  Created on: Apr 28, 2024
+ *      Author: user
+ */
+
+#ifndef SMARTAPP_H_
+#define SMARTAPP_H_
+
+void LTSystem();
+void BuzzerSound();
+void NotifyFunc();
+
+#define Room1 		1
+#define Room2 		2
+#define Room3 		3
+#define Room4 		4
+
+#define C_button 	14
+
+#define KPAD_STATE_ILDE 0xFF
+
+#endif /* SMARTAPP_H_ */
